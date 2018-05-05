db12a992f41dc8287d57bab27ea3326b0f25f751
Jun Gu <2dxgujun@gmail.com>
2018-05-05 16:01:49 +0800

    add foreground

--- host-stats.sh
+++ host-stats.sh
@@ -16,14 +16,31 @@ if
 [ -z "$SCALE6" ] || 
 [ -z "$SCALE7" ]
 then
-  SCALE0="[       ]"
-  SCALE1="[ǀ      ]"
-  SCALE2="[ǀǀ     ]"
-  SCALE3="[ǀǀǀ    ]"
-  SCALE4="[ǀǀǀǀ   ]"
-  SCALE5="[ǀǀǀǀǀ  ]"
-  SCALE6="[ǀǀǀǀǀǀ ]"
-  SCALE7="[ǀǀǀǀǀǀǀ]"
+  SCALE0="       "
+  SCALE1="ǀ      "
+  SCALE2="ǀǀ     "
+  SCALE3="ǀǀǀ    "
+  SCALE4="ǀǀǀǀ   "
+  SCALE5="ǀǀǀǀǀ  "
+  SCALE6="ǀǀǀǀǀǀ "
+  SCALE7="ǀǀǀǀǀǀǀ"
+fi
+
+SCALE0="#[fg=green]${SCALE0:0:2}#[fg=yellow]${SCALE0:2:3}#[fg=red]${SCALE0:5}"
+SCALE1="#[fg=green]${SCALE1:0:2}#[fg=yellow]${SCALE1:2:3}#[fg=red]${SCALE1:5}"
+SCALE2="#[fg=green]${SCALE2:0:2}#[fg=yellow]${SCALE2:2:3}#[fg=red]${SCALE2:5}"
+SCALE3="#[fg=green]${SCALE3:0:2}#[fg=yellow]${SCALE3:2:3}#[fg=red]${SCALE3:5}"
+SCALE4="#[fg=green]${SCALE4:0:2}#[fg=yellow]${SCALE4:2:3}#[fg=red]${SCALE4:5}"
+SCALE5="#[fg=green]${SCALE5:0:2}#[fg=yellow]${SCALE5:2:3}#[fg=red]${SCALE5:5}"
+SCALE6="#[fg=green]${SCALE6:0:2}#[fg=yellow]${SCALE6:2:3}#[fg=red]${SCALE6:5}"
+SCALE6="#[fg=green]${SCALE7:0:2}#[fg=yellow]${SCALE7:2:3}#[fg=red]${SCALE7:5}"
+
+if [ -z "$SCALE_BOUND_L" ]; then
+  SCALE_BOUND_L="["
+fi
+
+if [ -z "$SCALE_BOUND_R" ]; then
+  SCALE_BOUND_R="]"
 fi
 
 default_scale_max=7
@@ -47,7 +64,7 @@ main() {
   fi
   local scale=SCALE${scale_value}
 
-  echo "$available_mem $load_average ${!scale}"
+  echo "$available_mem $load_average $SCALE_BOUND_L${!scale}$SCALE_BOUND_R"
 }
 
 main
